7e07ae78fb97f1e0192b05a15909e71b09751722
unknown <hr.alphasquad01@gmail.com>
2022-11-22 19:16:37 +0500

    khk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Games from "./Games";
 function App() {
+  
   return (
 <>
 <Games/>
--- src/Games.js
+++ src/Games.js
@@ -8,7 +8,7 @@ export const Games = () => {
             </div>
             <div className='w-full max-[200px] '>
                 <div>
-                    <div className='max-w-[1024px] max-h-[24px] mx-auto mt-[70px]'>
+                    <div className='max-w-[1024px] max-h-[24px] mx-auto mt-[0px]'>
                         <h1 className='text-[24px] font-bold leading-6 text-gray-800 font-koh Santepheap'>Puzzle Game Test</h1>
                     </div>
                     <div className='max-w-[1024px] max-h-[16px] mx-auto mt-[20px]'>
@@ -18,104 +18,114 @@ export const Games = () => {
                 <div className='mt-5 max-w-[1024px] w-full h-full flex  content-center'>
                     <div className='max-w-[1024px] max-h-[48px]'>
                         <input type="number" placeholder='Number' className='mx-auto w-[360px] h-[48px] bg-gray-200 rounded-[4px] p-[16px] gap-[10px]'></input>
-                        </div>
-                        <div>
-                        < button className='w-[119px] h-[48px] bg-gray-200 rounded-[4px] pl-[32px] pr-[32px] pt-[16px] pb-[16px] bg-blue-600 ml-[28px] text-[16px] leading-4 font-koh Santepheap font-normal'>Submit</button>
-                        <button onclick="activateLasers()"> Activate Lasers</button>
-                        </div>
+                    </div>
+                    <div>
+                        < button onclick="myFunction()" className='w-[119px] h-[48px] bg-gray-200 rounded-[4px] pl-[32px] pr-[32px] pt-[16px] pb-[16px] bg-blue-600 ml-[28px] text-[16px] leading-4 font-koh Santepheap font-normal'>Submit</button>
+                        <script>
+                            function myFunction() {
+                            alert("Worng Number!")
+                            }
+                            </script>
+                    </div>
                     <div>
                         < button className='w-[119px] h-[48px] rounded-[4px] pl-[32px] pr-[32px] pt-[16px] pb-[16px] border-[1px] border-blue-600 text-blue-600 ml-[400px] text-[16px] leading-4 font-koh Santepheap font-normal'>Reset</button>
                     </div>
-                <div className='flex gap-[10px]'>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px] text-center'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>1</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>2</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>3</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>4</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>5</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>6</h1>
-                    </div>
 
                 </div>
-                {/* box two  */}
-                <div className='flex mt-[10px] gap-[10px]'>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>7</h1>
+
+                <div className='w-[1080px] h-[744px] bg-orange-100 border-orange-600 mt-[40px] p-[48px] rounded-[24px] mb-[40px]'>
+                    {/* box one  */}
+                    <div className='flex gap-[10px]'>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px] text-center'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>1</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>2</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>3</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>4</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>5</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>6</h1>
+                        </div>
+
                     </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>8</h1>
+                    {/* box two  */}
+                    <div className='flex mt-[10px] gap-[10px]'>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>7</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>8</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>9</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>10</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>11</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>12</h1>
+                        </div>
+
                     </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[18px] h-[32px] mt-[50px] ml-[53px] text-[32px] font-normal leading-8 text-white'>9</h1>
+                    {/* box three */}
+                    <div className='flex mt-[10px] gap-[10px]'><div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>13</h1>
                     </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>10</h1>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>14</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>15</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>16</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>17</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>18</h1>
+                        </div>
+
                     </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>11</h1>
+                    {/* box four  */}
+                    <div className='flex mt-[10px] gap-[10px]'>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>19</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>20</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>21</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>22</h1>
+                        </div>
+                        <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>23</h1>
+                        </div>
+
+                        <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
+                            <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>24</h1>
+                        </div>
                     </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>12</h1>
                     </div>
 
-                </div>
-                {/* box three */}
-                <div className='flex mt-[10px] gap-[10px]'><div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                    <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>13</h1>
-                </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>14</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>15</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>16</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>17</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>18</h1>
-                    </div>
-
-                </div>
-                {/* box four  */}
-                <div className='flex mt-[10px] gap-[10px]'>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>19</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>20</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>21</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-green-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>22</h1>
-                    </div>
-                    <div className='rounded-[16px] bg-blue-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>23</h1>
-                    </div>
-
-                    <div className='rounded-[16px] bg-yellow-500 w-[213px] h-[150px] p-[16px]'>
-                        <h1 className='w-[36px] h-[32px] mt-[50px] ml-[43px] text-[32px] font-normal leading-8 text-white'>24</h1>
-                    </div>
-                </div>
 
 
-
-
+           
             </div>
 
 
